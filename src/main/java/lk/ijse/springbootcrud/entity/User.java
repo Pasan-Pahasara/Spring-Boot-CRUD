11b07dbe14ba3f4f3b6454d166f95fefb16c3840
Pasan Pahasara <88943660+Pasan-Pahasara@users.noreply.github.com>
2023-03-06 01:40:14 +0530

    Added and FullyRefactored User Entity

---
+++ src/main/java/lk/ijse/springbootcrud/entity/User.java
@@ -0,0 +1,19 @@
+package lk.ijse.springbootcrud.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+public class User {
+    @Id
+    private int id;
+    private String name;
+    private String address;
+}

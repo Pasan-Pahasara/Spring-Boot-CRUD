4014fd76c615b15d07c0949fb506bc596579e746
Pasan Pahasara <88943660+Pasan-Pahasara@users.noreply.github.com>
2023-03-06 00:35:19 +0530

    initial commit

---
+++ src/test/java/lk/ijse/springbootcrud/SpringBootCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.springbootcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

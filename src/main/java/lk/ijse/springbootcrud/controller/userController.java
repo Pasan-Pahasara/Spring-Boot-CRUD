82ee4061e8eb516f2df9dac3782aa7a9acfd2ad0
Pasan Pahasara <88943660+Pasan-Pahasara@users.noreply.github.com>
2023-03-06 01:03:04 +0530

    introducing @PostMapping

--- src/main/java/lk/ijse/springbootcrud/controller/userController.java
+++ src/main/java/lk/ijse/springbootcrud/controller/userController.java
@@ -1,16 +1,18 @@
 package lk.ijse.springbootcrud.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "api/v1 /user")
 @CrossOrigin
 public class userController {
     @GetMapping("/getUser")
-    public String getUser(){
+    public String getUser() {
         return "ASUS VivoBook";
+    }
+
+    @PostMapping("/saveUser")
+    public String saveUser() {
+        return "User Saved..!";
     }
 }

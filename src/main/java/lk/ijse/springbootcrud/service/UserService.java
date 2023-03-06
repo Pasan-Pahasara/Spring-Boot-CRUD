5cf248510295b70e07ce86970901dc6fc0cdbf27
Pasan Pahasara <88943660+Pasan-Pahasara@users.noreply.github.com>
2023-03-06 11:14:55 +0530

    FullyRefactored getAllUsers in Service

--- src/main/java/lk/ijse/springbootcrud/service/UserService.java
+++ src/main/java/lk/ijse/springbootcrud/service/UserService.java
@@ -4,10 +4,12 @@ import lk.ijse.springbootcrud.dto.UserDTO;
 import lk.ijse.springbootcrud.entity.User;
 import lk.ijse.springbootcrud.repo.UserRepo;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.List;
 
 @Service
 @Transactional
@@ -20,5 +22,10 @@ public class UserService {
     public UserDTO saveUser(UserDTO userDTO){
         repo.save(modelMapper.map(userDTO, User.class));
         return userDTO;
+    }
+
+    public List<UserDTO> getAllUsers(){
+        List<User> userList = repo.findAll();
+        return modelMapper.map(userList,new TypeToken<List<User>>(){}.getType());
     }
 }
